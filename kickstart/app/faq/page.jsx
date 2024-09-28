7bf620a9e42332b307837f314ae2e4f76dbbe969
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2024-09-28 15:13:28 +0300

    meta added

--- kickstart/app/faq/page.jsx
+++ kickstart/app/faq/page.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import FAQ from "./_components/Faq";
 
+export async function generateMetadata() {
+  return {
+    title: `KickStart - FAQ`,
+    description: `KickStart FAQ page`,
+  };
+}
+
 export default function FAQPage() {
   return (
     <div className="dark:bg-slate-950 min-h-screen w-full flex justify-center items-center">
